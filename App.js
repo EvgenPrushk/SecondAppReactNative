8bee6783cbeefdc22299059a9734eeb55c88f5a3
EvgenPrushk <ogmprushak@mail.ru>
2021-07-02 17:08:42 +0300

    feat: new functional

--- App.js
+++ App.js
@@ -2,22 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
-import Group from "./components/Group";
+import Group from "./components/Group/Group.jsx";
 
 export default function App() {
   return (
     <Container>
-      <Group
-        title="14 september"
-        items={[
-          (time: "15:30"),
-          (diagnosis: "pulpit"),
-          (user: {
-            fullname: "Alex Byi",
-          }),
-          (avatar: ""),
-        ]}
-      />
+      <Group title="14 september" items={{
+        time: '15:30',
+        diagnosis: 'Pulpit',
+        user: {
+          fullname: 'Alex Byi',
+        }
+      }} ></Group>
     </Container>
   );
 }
